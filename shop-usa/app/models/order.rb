15376bce079d6d788c959cc954c9114aafd8ad65
Carl <carl.truong555@gmail.com>
2016-10-21 01:14:49 +0000

    in progress

--- shop-usa/app/models/order.rb
+++ shop-usa/app/models/order.rb
@@ -1,3 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
+  
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :shipping_charge, presence: true
 end
