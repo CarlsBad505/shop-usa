14c2f4097793d96b6d109c12d9ae0a1a0729a94e
Carl <carl.truong555@gmail.com>
2016-10-29 21:32:40 +0000

    change seed data

--- shop-usa/db/seeds.rb
+++ shop-usa/db/seeds.rb
@@ -1,36 +1,42 @@
 # Create Users
-5.times do
-  User.create!(
-    email: Faker::Internet.email,
-    password: Faker::Internet.password(minimum_length = 6)
-  )
-end
+# 5.times do
+#   User.create!(
+#     email: Faker::Internet.email,
+#     password: Faker::Internet.password(minimum_length = 6)
+#   )
+# end
 
+# User.create!(
+#     email: 'carl@tabrific.com',
+#     password: 'Yellow123',
+#     admin: true
+#   )
+  
 User.create!(
-    email: 'carl@tabrific.com',
-    password: 'Yellow123',
+    email: 'peter.sidles@courierusallc.com',
+    password: 'temporary9137',
     admin: true
   )
 
-users = User.all
+# users = User.all
 
-20.times do
-  Order.create!(
-    description: Faker::Lorem.sentence(word_count = 6),
-    shipping_charge: Faker::Number.number(3),
-    track_package: Faker::Internet.url,
-    user: users.sample
-  )
-end
+# 20.times do
+#   Order.create!(
+#     description: Faker::Lorem.sentence(word_count = 6),
+#     shipping_charge: Faker::Number.number(3),
+#     track_package: Faker::Internet.url,
+#     user: users.sample
+#   )
+# end
 
-5.times do
-   Order.create!(
-    description: Faker::Lorem.sentence(word_count = 6),
-    shipping_charge: Faker::Number.number(3),
-    track_package: Faker::Internet.url,
-    user: User.last
-  )
-end
+# 5.times do
+#   Order.create!(
+#     description: Faker::Lorem.sentence(word_count = 6),
+#     shipping_charge: Faker::Number.number(3),
+#     track_package: Faker::Internet.url,
+#     user: User.last
+#   )
+# end
   
 puts "Seed Finished"
 puts "#{User.count} users created"
